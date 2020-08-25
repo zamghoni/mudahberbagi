a88f1abd750a593ac66a27a56aae619c211eaa8d
zamghoni <12171426@bsi.ac.id>
2020-08-26 00:51:11 +0700

    Kategori Done

--- application/views/admin/modkategori/edit.php
+++ application/views/admin/modkategori/edit.php
@@ -1,27 +1,27 @@
 <div class="row">
-<div class="col-sm-12">
-<div class="widget-box">
-<div class="widget-header">
-<h4 class="widget-title"><?php echo $sub; ?></h4>
-</div>
-<div class="widget-body">
-<div class="widget-main no-padding">
-  <!--Form-->
-  <form method="post" action="<?php echo site_url('kategori/update/'.$data['id_kategori']) ?>">
-  <fieldset>
+  <div class="col-sm-12">
+    <div class="widget-box">
+      <div class="widget-header">
+        <h4 class="widget-title"><?php echo $sub; ?></h4>
+      </div>
+      <div class="widget-body">
+        <div class="widget-main no-padding">
+          <!--Form-->
+          <form method="post" action="<?php echo site_url('kategori/update/'.$data['id_kategori']) ?>">
+            <fieldset>
 
-  <label>Kategori</label><br>
-  <input type="text" name="nama_kategori" class="form-control" placeholder="Masukan Nama Kategori" value="<?php echo $data['nama_kategori'] ?>" required oninvalid="this.setCustomValidity('Nama Kategori Harus Di Isi')" oninput="setCustomValidity('')"><p></p>
+              <label>Kategori</label><br>
+              <input type="text" name="nama_kategori" class="form-control" placeholder="Masukan Nama Kategori" value="<?php echo $data['nama_kategori'] ?>" required oninvalid="this.setCustomValidity('Nama Kategori Harus Di Isi')" oninput="setCustomValidity('')"><p></p>
 
-  <div class="form-actions center">
-  <button class="btn btn-info" type="submit"><i class="ace-icon fa fa-check bigger-110"></i>Perbarui</button>
-  <a href="<?php echo site_url('kategori') ?>"><button class="btn btn-warning" type="button"><i class="ace-icon fa fa-undo bigger-110"></i>Batal</button></a>
+              <div class="form-actions text-center">
+                <a href="<?php echo site_url('kategori') ?>"><button class="btn btn-danger" type="button"><i class="ace-icon fa fa-undo bigger-110"></i> Batal</button></a>
+                <button class="btn btn-primary" type="submit"><i class="ace-icon fa fa-check bigger-110"></i> Perbarui</button>
+              </div>
+            </fieldset>
+          </form>
+          <!--EndForm-->
+        </div>
+      </div>
+    </div>
   </div>
-  </fieldset>
-  </form>
-  <!--EndForm-->
-</div>
-</div>
-</div>
-</div>
 </div>
